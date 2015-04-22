1222d33c8fb76ad5b112d7058da4bb733c8ce41f
yaozhenfa <976691141@qq.com>
2015-04-22 17:47:23 +0800

    Create Project

---
+++ XamarinViewPager/XamarinViewPager.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.View;
+
+namespace XamarinViewPager
+{
+    public class XamarinViewPager : ViewPager
+    {
+        public static String TAG = "XamarinViewPager";
+    }
+}
